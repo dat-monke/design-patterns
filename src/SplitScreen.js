ca5ed26c45375976960ef59020a01dae0d7ab7bf
David Taing <dtaing1@uci.edu>
2022-07-19 13:16:12 -0700

    working ratio

---
+++ src/SplitScreen.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+    display: flex
+`
+
+// By having flex == 1, you are saying for it to take equal amount of space
+// With props => props.weight, it will allow the leftweight/rightweight to be overwritten
+const Pane = styled.div`
+    flex: ${props => props.weight} 
+`
+
+export const SplitScreen = ({
+    left: Left,
+    right: Right,
+    leftWeight  = 1,
+    rightWeight = 1
+}) => {
+    return (
+        <Container> 
+            <Pane weight={leftWeight}> 
+                <Left />
+            </Pane>
+            <Pane weight={rightWeight}> 
+                <Right /> 
+            </Pane>
+        </Container>
+    )
+}
